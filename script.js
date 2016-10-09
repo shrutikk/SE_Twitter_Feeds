9319a32a6961c8ad3427624fb23cf4988dc1d42c
Shruti <shruti@localhost.localdomain>
2016-10-09 15:49:04 -0400

    UI done

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 function loadData(){
 
-	var getInfoReq = new XMLHttpRequest();
-	var infoUrl = "https://stream.twitter.com/1.1/statuses/sample.json?oauth_consumer_key=1oi7DeHwoDQnHuUqe9bttt8bf&oauth_token=784482147981819904-2xfQZTz57q0A8gV5e1v95C7w5EGNaMu&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1475948690&oauth_nonce=TXshkc&oauth_version=1.0&oauth_signature=oN9EsSJC64N/N3fL85W28F3C08M=";
-
-	getInfoReq.onreadystatechange = function(){
-		if (this.readyState == 4 && this.status == 200) {
-		console.log(this.responseText);	
-		}
-	}
-	getInfoReq.open('GET', infoUrl, true);
-	getInfoReq.send();
-
+		$("body").tooltip({ selector: '[data-toggle=tooltip]' });
+		createBindings();
+ 
 }
+
+var kbind = {};
+function createBindings(){
+
+		var tweets = tweet;
+		var tweetFields = [];
+		tweets.forEach(function(tweet, index){
+			var newTweet = {};
+			newTweet.name = tweet.user.name;
+			newTweet.username = tweet.user.screen_name;
+			newTweet.text = tweet.text;
+			newTweet.user_img = tweet.user.profile_image_url;
+			newTweet.favorite_count = tweet.favorite_count;
+			newTweet.retweet_count = tweet.retweet_count;
+			newTweet.hasImage = tweet.hasImage;
+			newTweet.imgUrl = tweet.imgUrl;
+			tweetFields.push(newTweet);
+		});
+		kbind['tweets'] = tweetFields;
+		console.log(kbind);
+		ko.applyBindings(kbind);
+}
+
